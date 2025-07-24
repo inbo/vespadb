2693bf9fe20aa11559e631442330fd67170ccacc
Sander Devisscher <sander.devisscher@inbo.be>
2025-07-24 18:16:03 +0200

    Create login_vespadb.R
    #1

---
+++ R/login_vespadb.R
@@ -0,0 +1,63 @@
+#' Login to VespaDB API to fetch fields that are not publicly available
+#'
+#' This function allows you to login to the API and returns the required
+#' csrftoken needed to fetch information that is not publicly available.
+#'
+#' @param username The username to login with
+#' @param password The password to login with
+#' @param domain The domain to login to. Default is "uat" for the UAT
+#'   environment.
+#' @param verbose Whether to print the response from the API. Default is FALSE.
+#'
+#' @return The path to an authentication cookie.
+#'
+#' @examples
+#' \dontrun{
+#' get_vespadb_obs(municipality = 215,
+#'                           auth = login_vespadb("username",
+#'                                                "password"))
+#' }
+login_vespadb <- function(username, password, domain = c("uat", "production")){
+  api_url <- switch(rlang::arg_match(domain),
+                    uat = "https://uat-db.vespawatch.be/login/",
+                    production = "https://db.vespawatch.be/login/")
+
+  if(missing(username)) {
+    if (Sys.getenv("VESPADB_USER") != "") {
+      username <- Sys.getenv("VESPADB_USER")
+      cli::cli_alert_info("Found credentials for {username}")
+    } else {
+      username <- readline(prompt = "Please enter your vespadb username: ")
+    }
+  }
+
+  if(missing(password)){
+    if (Sys.getenv("VESPADB_PWD") != "") {
+      password <- Sys.getenv("VESPADB_PWD")
+    } else {
+      password <- askpass::askpass()
+    }
+  }
+
+  assertthat::assert_that(assertthat::is.string(username))
+  assertthat::assert_that(assertthat::is.string(password))
+
+  # Set path to store cookie on
+  path_to_cookie <- tempfile()
+
+  # Perform login POST request
+  response <-
+    httr2::request(api_url) %>%
+    httr2::req_method("POST") %>%
+    httr2::req_body_json(list(username = username, password = password)) %>%
+    httr2::req_retry() %>%
+    httr2::req_cookie_preserve(path = path_to_cookie) %>%
+    httr2::req_perform()
+
+
+
+  # Return error if login wasn't succesful
+  httr2::resp_check_status(response)
+
+  invisible(path_to_cookie)
+}
